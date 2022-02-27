ac28198067946674a4c488ef399f7ac4f8c797cd
unknown <robkuch123@gmail.com>
2022-02-27 22:35:43 +0300

    first commit

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello horld!");
+        //my new one... to practice what there is and how it is going
+    }
+}

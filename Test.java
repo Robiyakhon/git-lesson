414e5516c5e1b431d55a938b9251d731e8dac6e6
unknown <robkuch123@gmail.com>
2022-02-27 22:55:41 +0300

    second commit

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello horld!");
         //my new one... to practice what there is and how it is going
+        // i changed so want to know what is happening
     }
 }
